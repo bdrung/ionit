c9eaa13d337843aa786e96a6fc1cf633ea69716c
Benjamin Drung <bdrung@posteo.de>
2022-03-04 16:01:51 +0100

    Release ionit 0.4.1
    Signed-off-by: Benjamin Drung <bdrung@posteo.de>

--- setup.py
+++ setup.py
@@ -37,7 +37,7 @@ if __name__ == "__main__":
 
     setup(
         name="ionit",
-        version="0.4.0",
+        version="0.4.1",
         description="Render configuration files from Jinja templates",
         long_description=LONG_DESCRIPTION,
         long_description_content_type="text/markdown",
